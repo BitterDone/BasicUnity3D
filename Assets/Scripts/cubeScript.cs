3b4bfa43b38d1984a21359ecb24d45478e175c33
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-10-30 16:35:26 -0500

    before add Kafka via NuGet

---
+++ Assets/Scripts/cubeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cubeScript : MonoBehaviour
+{
+	public Text redPanelText;
+    // Start is called before the first frame update
+    void Start()
+    {
+		string panelText = "sample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+		panelText += "\nsample kafka message";
+
+		redPanelText.text = panelText;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
